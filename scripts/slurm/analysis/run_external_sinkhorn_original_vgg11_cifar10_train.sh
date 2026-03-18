9ca7271643c90b391e2b1cde5e8e7d80df3976bb
Maciej Lodzinski <mf.lodzinski@gmail.com>
2026-03-18 18:57:21 +0100

    asd

--- scripts/slurm/analysis/run_external_sinkhorn_original_vgg11_cifar10_train.sh
+++ scripts/slurm/analysis/run_external_sinkhorn_original_vgg11_cifar10_train.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --partition=general
 #SBATCH --qos=short
-#SBATCH --time=06:00:00
+#SBATCH --time=01:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=16GB
+#SBATCH --mem=4GB
 #SBATCH --mail-type=END,FAIL
 #SBATCH --output=slurm_external_sinkhorn_orig_vgg11_cifar10_train_%j.out
 #SBATCH --error=slurm_external_sinkhorn_orig_vgg11_cifar10_train_%j.err
