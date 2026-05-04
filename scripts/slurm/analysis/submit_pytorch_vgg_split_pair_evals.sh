9a3217c11437fd5dc07de5c47ca70761003cf6b5
Maciej Lodzinski <mf.lodzinski@gmail.com>
2026-05-04 21:36:55 +0200

    asd

---
+++ scripts/slurm/analysis/submit_pytorch_vgg_split_pair_evals.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -euo pipefail
+
+SCRIPT="scripts/slurm/analysis/run_pytorch_vgg_split_pair_eval.sh"
+
+sbatch "${SCRIPT}" "100/100"
+sbatch "${SCRIPT}" "80/120"
+sbatch "${SCRIPT}" "30/170"
+sbatch "${SCRIPT}" "8/192"
+sbatch "${SCRIPT}" "6/194"
+sbatch "${SCRIPT}" "0/200"
+sbatch "${SCRIPT}" "independent"
